c0f4aa2d5acf05b9289277bd264ce5929f1d8748
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-05-30 16:25:47 -0700

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 var buttonEl = document.querySelector(".btn");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener('click', function() {
-    alert("button clicked");
-})
+
+
+function createTaskHandler() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener('click', createTaskHandler)
